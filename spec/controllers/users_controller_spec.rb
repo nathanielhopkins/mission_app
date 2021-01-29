3bdeb72927369d6f2e29a01284a31589c83e14ed
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-01-29 13:17:36 -0800

    begin writing tests for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,86 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  describe "GET #new" do
+
+    it "returns a successful response" do
+      get :new, {}
+      expect(response).to be_successful
+    end
+
+    it "assings a new User to @user" do
+      new_user = User.new
+      get :new, {}
+      expect(assings[:user]).to eq(new_user)
+    end
+
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "POST #create" do
+    context "with invalid params" do
+      it "validates the presence of the user's username and password" do
+        post :create, params: { user: { username: 'bob49'}}
+        expect(response).to render_template('new')
+        expect(flash[:errors]).to be_present
+      end
+
+      it "validates that the password is at least 6 characters long" do
+        post :create, params: { user: { username: 'bob49', password: 'buns'}}
+        expect(response).to render_template('new')
+        expect(flash[:errors]).to eq(["Password is too short (minimum is 6 characters)"])
+      end
+    end
+
+    context "with valid params" do
+      it "redirects user to users index on success" do
+        post :create, params: { user: { username: 'bob49', password: 'bobpassword'}}
+        expect(response).to redirect_to(user_url(User.last))
+      end
+    end
+  end
+
+  describe 'GET #index' do
+
+    it "returns a successful response" do
+      get :index, {}
+      expect(response).to be_successful
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
+  describe 'GET #show' do
+
+    it 'assings the requested user to @user' do
+      user = Factory(:user)
+      get :show, id: user
+      expect(assings[:user]).to eq(user)  
+    end
+
+    it 'renders the show template' do
+      get :show, id: Facory(:user)
+      expect(response).to render_template("show")
+    end
+  end
+
+  describe 'GET #edit' do
+
+    it "should render edit template" do
+      get :edit, id: Factory(:user)
+      expect(response).to render_template("edit")
+    end
+  end
+
+  describe 'POST #edit' do
+  end
+
+  describe 'DELETE #destroy' do
+  end
 end
