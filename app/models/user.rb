a4620294314b2ec787444dea4b856ca48ff15f18
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-01 12:48:26 -0800

    write #is_password?, #reset_session_token!, and #self.find_by_credentials in User to pass specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,23 @@ class User < ApplicationRecord
     SecureRandom::urlsafe_base64(16)
   end
 
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token
+    self.save
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
   private
   def ensure_session_token
     self.session_token ||= User.generate_session_token
