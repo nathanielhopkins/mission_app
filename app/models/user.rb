4182d6aafa5ebc7c1a26aebadd06ac3634f3c963
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-01 12:42:33 -0800

    write validations to pass tests; add #password=, self.generate_session_token, and private method ensure_session_token to User model to allow creation of User instance

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  validates :username, :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true}
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
 end
