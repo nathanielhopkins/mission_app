865beef8f30bd8fc6e7c33e8c095f1a8037cbf45
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-01 15:54:48 -0800

    create show view for users; write #show in UsersController to pass all specs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,16 @@ class UsersController < ApplicationController
     render :index    
   end
 
+  def show
+    @user = User.find_by(id: params[:id]) 
+    
+    if @user
+      render :show
+    else
+      redirect_to users_url
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:username, :password)
