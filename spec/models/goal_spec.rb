5dc5b73b94793fa1c9aae0caeb2d74ba375f0b50
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-02 13:48:21 -0800

    write validation and association specs for goals

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Goal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:user_id) }
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:private) }
+    it { should validate_presence_of(:completed) }
+    it { should validate_presence_of(:username) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:user) }
+  end
 end
