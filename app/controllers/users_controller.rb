8038f22d423d79a1bd80eaf99ff450155da06789
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-02 12:05:21 -0800

    add root to in routes.rb; add before_action require login to UsersController except new and create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :require_no_login!, only: [:new, :create]
+  before_action :require_login!, except: [:new, :create]
   
   def new
     @user = User.new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'users#index'
+
   resources :users
   resource :session, only: [:new, :create, :destroy]
 end
