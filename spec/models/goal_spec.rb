cdd5fa6ccef1dd378e32727cf45080c89f896808
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-02 14:28:20 -0800

    change goal_spec: remove presence validations for private and completed; add validates length of title is at least 6

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -5,8 +5,7 @@ RSpec.describe Goal, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:title) }
-    it { should validate_presence_of(:private) }
-    it { should validate_presence_of(:completed) }
+    it { should validate_length_of(:title).is_at_least(6) }
   end
 
   describe 'associations' do
