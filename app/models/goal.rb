5cb82ba46faa84359671e5d6d97844657223d6f6
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-02 13:53:04 -0800

    write validations and associations in Goal model to pass all specs

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,2 +1,10 @@
 class Goal < ApplicationRecord
+  validates :user_id, :title, :private, :completed, presence: true
+
+  belongs_to(
+    :user,
+    class_name: "User",
+    foreign_key: :user_id,
+    primary_key: :id
+  )
 end
