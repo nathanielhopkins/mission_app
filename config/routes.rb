1b36716eb50a58680270639c9188f6546b93e2c3
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-02 15:10:36 -0800

    add resources for goals in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: 'users#index'
 
-  resources :users
+  resources :users do
+    resources :goals, only: [:new]
+  end
+  resources :goals, except: [:new, :index]
   resource :session, only: [:new, :create, :destroy]
 end
