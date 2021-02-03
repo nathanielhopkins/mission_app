1d034cd2d9e5f912a6d4004cbbabdbd539b53f06
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-03 14:52:57 -0800

    write #update in GoalsController to pass all specs

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -44,6 +44,22 @@ class GoalsController < ApplicationController
     end
   end
 
+  def update
+    @goal = Goal.find_by(id: params[:id])
+    redirect_to user_url(current_user) if !@goal
+
+    if @goal.user_id == current_user.id
+      if @goal.update_attributes(goal_params)
+        redirect_to goal_url(@goal)
+      else
+        flash.now[:errors] = @goal.errors.full_messages
+        render :edit
+      end
+    else
+      redirect_to goal_url(@goal)
+    end
+  end
+
   private
   def goal_params
     params.require(:goal).permit(:title, :details, :private, :completed)
