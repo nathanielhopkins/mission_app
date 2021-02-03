f490746395352b9d87d225f8fd6fc2168d9b27dc
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-03 11:28:25 -0800

    finish writing specs for "GET #new" and "POST #create" in goals_controller_spec

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
-  let(:bob49) { FactoryBot.create(:user) } 
+  subject(:bob49) { FactoryBot.create(:user) } 
     
   describe 'GET #new' do
     context 'when logged in' do
@@ -34,9 +34,40 @@ RSpec.describe GoalsController, type: :controller do
 
   describe 'POST #create' do
     context 'when logged in' do
+      before(:each) do
+        allow(controller).to receive(:current_user) { bob49 }
+      end
+
+      context 'with valid params' do
+        it 'returns a successful response' do
+          post :create, params: { goal: { title: 'test', details: 'this is only a test'}}
+          expect(response).to be_successful
+        end
+
+        it 'redirects to created Goal show page' do
+          post :create, params: { goal: { title: 'test', details: 'this is only a test'}}
+          expect(response).to redirect_to(goal_url(Goal.last))
+        end
+      end
+
+      context 'with invalid params' do
+        it 'validates the presence of title and url' do
+          post :create, params: { goal: { title: 'invld' } }
+          expect(response).to render_template('new')
+          expect(flash[:errors]).to be_present
+        end
+      end
     end
 
     context 'when logged out' do
+      before(:each) do
+        allow(controller).to receive(:current_user) { nil }
+      end
+
+      it 'redirects to the login page' do
+        post :create, params: { goal: {} }
+        expect(response).to redirect_to(new_user_url)
+      end
     end
   end
 end
