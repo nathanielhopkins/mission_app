dc1a7795521a4d2f343a254badfcdf2b7ffe15e1
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-04 12:08:35 -0800

    write skeleton specs for goals_privacy_spec.rb

---
+++ spec/features/goals_privacy_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature "privacy of goals" do
+  given!(:user) { FactoryBot.create(:user) }
+  given!(:other_user) { FactoryBot.create(:other_user) }
+
+  describe 'public goals' do
+    given!(:goal) { FactoryBot.create(:goal, user: user) }
+
+    scenario "should create goals as public by default"
+
+    scenario "allows other users to see public goals"
+  end
+
+  describe "private goals" do
+    given!(:private_goal) { FactoryBot.create(:goal, user: user, private: true) }
+
+    scenario "allows creation of private goals"
+
+    scenario "hides private goals when logged out"
+
+    scenario "hides private goals from other users"
+  end  
+end 
