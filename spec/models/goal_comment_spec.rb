fd93b3639c502e1aee1cee0974f0acec7f2bdfdb
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-08 14:42:53 -0800

    write model spec for GoalComment

--- spec/models/goal_comment_spec.rb
+++ spec/models/goal_comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GoalComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:author_id)}
+    it { should validate_presence_of(:goal_id)}
+    it { should validate_presence_of(:body)}
+  end
+
+  describe 'associations' do
+    it { should belong_to(:author) }
+    it { should belong_to(:goal) }
+  end
 end
