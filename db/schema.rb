280cdccc737a4008521ba08e5d07500b423663c1
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-08 14:40:13 -0800

    generate model GoalsComment; write and run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_08_194012) do
+ActiveRecord::Schema.define(version: 2021_02_08_223723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "goal_comments", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "goal_id", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_goal_comments_on_author_id"
+    t.index ["goal_id"], name: "index_goal_comments_on_goal_id"
+  end
 
   create_table "goals", force: :cascade do |t|
     t.integer "user_id", null: false
