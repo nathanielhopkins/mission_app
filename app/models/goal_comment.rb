d25dc41ad6b66367feb3605a78afc59635877843
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-08 14:47:59 -0800

    write associations for GoalComment to pass all specs

--- app/models/goal_comment.rb
+++ app/models/goal_comment.rb
@@ -1,5 +1,17 @@
 class GoalComment < ApplicationRecord
   validates :author_id, :goal_id, :body, presence: true
 
-  
+  belongs_to(
+    :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :goal,
+    class_name: "Goal",
+    foreign_key: :goal_id,
+    primary_key: :id
+  )
 end
