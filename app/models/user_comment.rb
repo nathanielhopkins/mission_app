e7e676e3b1486e8892ac57a761ce9172e361285a
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-08 12:22:07 -0800

    write associations in UserComment to pass all specs

--- app/models/user_comment.rb
+++ app/models/user_comment.rb
@@ -1,3 +1,17 @@
 class UserComment < ApplicationRecord
   validates :author_id, :user_id, :body, presence: true
+
+  belongs_to(
+    :author,
+    class_name: "User",
+    foreign_key: :author_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :user,
+    class_name: "User",
+    foreign_key: :user_id,
+    primary_key: :id
+  )
 end
