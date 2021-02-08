f64e5012e261e1219331aca707ee3fe8bc41b1fc
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-08 15:03:29 -0800

    write GoalCommentsController to pass all specs

--- app/controllers/goal_comments_controller.rb
+++ app/controllers/goal_comments_controller.rb
@@ -1,2 +1,21 @@
 class GoalCommentsController < ApplicationController
+  before_action :require_login!
+
+  def create
+    @comment = GoalComment.new(goal_comment_params)
+    @comment.author_id = current_user.id
+
+    if @comment.save
+      flash[:notices] = ["Comment added!"]
+    else
+      flash[:errors] = @comment.errors.full_messages
+    end
+
+    redirect_back fallback_location: users_url
+  end
+
+  private
+  def goal_comment_params
+    params.require(:comment).permit(:goal_id, :body)
+  end
 end
