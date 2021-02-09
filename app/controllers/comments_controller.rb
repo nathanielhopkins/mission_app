504a8bfc381f750e9107f6dd836bc8158b615920
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-09 12:08:46 -0800

    write CommentsController to pass all specs

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+  before_action :require_login!
+  
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+
+    if @comment.save
+      flash[:notices] = ["Comment added!"]
+    else
+      flash[:errors] = @comment.errors.full_messages
+    end
+
+    redirect_back fallback_location: users_url
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :commentable_id, :commentable_type)
+  end
 end
