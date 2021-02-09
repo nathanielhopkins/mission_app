720bee068ea3789b0884a85ed7d426e4aa66718b
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-09 12:11:26 -0800

    remove deprecated resources for user_comments and goal_comments in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :new, :create]
   resources :goals
   resources :comments, only: [:create]
-  resources :user_comments, only: [:create]
-  resources :goal_comments, only: [:create]
   resource :session, only: [:new, :create, :destroy]
 end
