a33db1ae2cc4edf22645b0e81ace71c0cd6c28c3
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-09 12:48:06 -0800

    write cheer_spec.rb to test for validations and associations in Cheer model

--- spec/models/cheer_spec.rb
+++ spec/models/cheer_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Cheer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_uniqueness_of(:goal_id).scoped_to(:giver_id) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:giver) }
+    it { should belong_to(:goal) }
+  end
 end
