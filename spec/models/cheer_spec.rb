29aa555992fce3cc9fd5373d551578cbdaa909f3
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-09 13:08:37 -0800

    refactor the dummy instances in cheer_spec to be subject/let at top of spec

--- spec/models/cheer_spec.rb
+++ spec/models/cheer_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Cheer, type: :model do
+  let(:user) { User.create!(username: 'bob49', password: 'bobpassword') }
+  let(:g1) { Goal.create!(user_id: user.id, title: 'Goal user.id', completed: true) }
+  subject(:cheer) { Cheer.create!(giver_id: user.id, goal_id: g1.id)}
+  
   describe 'validations' do
-    before :each do
-      Cheer.create(giver_id: 1, goal_id: 1)
-    end
     it { should validate_uniqueness_of(:goal_id).scoped_to(:giver_id) }
   end
 
