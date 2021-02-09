0328952e8a2e3e86006e70964e3499f882558268
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-09 12:52:07 -0800

    add seed cheer to validation tests in cheer_spec so that uniqueness validation has a record to test off of

--- spec/models/cheer_spec.rb
+++ spec/models/cheer_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Cheer, type: :model do
   describe 'validations' do
+    before :each do
+      Cheer.create(giver_id: 1, goal_id: 1)
+    end
     it { should validate_uniqueness_of(:goal_id).scoped_to(:giver_id) }
   end
 
