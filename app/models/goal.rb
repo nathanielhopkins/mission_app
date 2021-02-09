b19f7c4801026d1fc565ad629142ef296b63d9f3
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-09 13:10:40 -0800

    refactor associations in goal.rb and add has_many :cheers association

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -4,10 +4,7 @@ class Goal < ApplicationRecord
   validates :user_id, presence: true
   validates :title, presence: true, length: { minimum: 6 }
 
-  belongs_to(
-    :user,
-    class_name: "User",
-    foreign_key: :user_id,
-    primary_key: :id
-  )
+  belongs_to :user
+
+  has_many :cheers
 end
