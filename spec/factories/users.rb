33fe74e240feffa30ed6d7ea235db0546284e060
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-02-09 15:01:09 -0800

    add :random_user to User FactoryBot

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,13 @@
+require "faker"
+
 FactoryBot.define do
   factory :user do
     username { "bob49" }
     password { "bobpassword" }
+
+    factory :random_user do
+      username { Faker::Internet.user_name }
+      password { "password" }
+    end
   end
 end
